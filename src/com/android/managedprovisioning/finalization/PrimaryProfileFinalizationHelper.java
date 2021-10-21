736996a7455f7757e4cd0b9dc9b8d3cb7c7cdcc7
Oliver Scott <olivercscott@gmail.com>
2021-10-21 03:07:07 -0400

    Allow provisioning work profiles without a profile owner
    Change-Id: I95e4fa8d21290f52421600c9988c5ce1ffcf2895

--- src/com/android/managedprovisioning/finalization/PrimaryProfileFinalizationHelper.java
+++ src/com/android/managedprovisioning/finalization/PrimaryProfileFinalizationHelper.java
@@ -40,7 +40,7 @@ class PrimaryProfileFinalizationHelper {
     PrimaryProfileFinalizationHelper(Account migratedAccount, UserHandle managedUserHandle,
             String mdmPackageName) {
         mMigratedAccount = migratedAccount;
-        mMdmPackageName = checkNotNull(mdmPackageName);
+        mMdmPackageName = mdmPackageName;
         mManagedUserHandle = checkNotNull(managedUserHandle);
     }
 
